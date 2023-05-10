4f1c309140ad898c2dd281bd200e66f5f7c75d04
Vytautas Valutis <dnc908@gmail.com>
2023-05-10 14:48:55 +0300

    bd work err

--- gofunds/app/Http/Controllers/MoneyController.php
+++ gofunds/app/Http/Controllers/MoneyController.php
@@ -3,62 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Money;
-use App\Http\Requests\StoreMoneyRequest;
-use App\Http\Requests\UpdateMoneyRequest;
+use Illuminate\Http\Request;
+use App\Models\History;
 
 class MoneyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(Request $request, History $history)
+    {
+        dd($request->value, $request->user_id, $history->id);
+    }
+
+    public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreMoneyRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Money $money)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Money $money)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateMoneyRequest $request, Money $money)
+    public function update(Request $request, Money $money)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Money $money)
     {
         //
--- gofunds/resources/views/front/donate.blade.php
+++ gofunds/resources/views/front/donate.blade.php
@@ -1,7 +1,8 @@
 @if($user_status)
-<form action="" method="post">
+<form action="{{route('money-create', $hist)}}" method="post">
     <button type="submit" class="btn btn-outline-primary">Donate :</button>
     <input name="value" type="text" value="0.00">
+    <input name="user_id" type="hidden" value={{$user_status->id}}>
     @csrf
     @method('put')
 </form>
